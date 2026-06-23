c8038d9e47768c83ccafcc2e4c406ff6b83bc311
Davis Vang <165615909+DavisVang@users.noreply.github.com>
2026-06-23 14:16:48 -0500

    Update customers-who-never-order.sql

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -4,3 +4,5 @@ left join orders o1
     on  c1.id = o1.customerId
 where o1.customerID is null;
 
+-- Were selecting the name to figure out WHO we want to see, and it's from the customer table. We do a left join so it only grabs the orders from the order table. 
+-- We make sure it's the id's that are matching and we use the WHERE clause to return the names that didn't order.
