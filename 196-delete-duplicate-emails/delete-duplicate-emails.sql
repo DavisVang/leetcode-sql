6cc05ac199fd01c938a135370560e7b7d19fb05c
Davis Vang <165615909+DavisVang@users.noreply.github.com>
2026-07-01 15:49:58 -0500

    Update delete-duplicate-emails.sql

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -4,3 +4,5 @@ from Person p1
 join Person p2 
 on p1.email = p2.email 
 and p1.id > p2.id;
+
+-- We do a self join to comapre the rows where email equal but if the id is greater, we delete the duplicate. 
