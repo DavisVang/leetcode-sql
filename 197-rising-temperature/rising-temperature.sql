928f2f21fcb37a17298d8d5e07c151562979e81d
Davis Vang <165615909+DavisVang@users.noreply.github.com>
2026-07-02 17:48:15 -0500

    Update rising-temperature.sql

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -4,3 +4,5 @@ from Weather w1
 join Weather w2 
 on w1.recordDate = date_add(w2.recordDate, interval 1 day)
 where w1.temperature > w2.temperature;
+
+-- We do a self join to make sure w1 matches the row to the day after and then it returns the id's if the tempature is higher then yesterday. 
