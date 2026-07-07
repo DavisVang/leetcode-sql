e179e21810eaded7c2d2c7369721dbb48976c52b
Davis Vang <165615909+DavisVang@users.noreply.github.com>
2026-07-06 20:10:12 -0500

    Update game-play-analysis-i.sql

--- 1179-game-play-analysis-i/game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -2,3 +2,6 @@ select player_id, min(event_date) as first_login
 from Activity 
 group by player_id 
 having (count(player_id) >= 1);
+
+-- We select player id and the mimimum of event_date as our output where it needs to print out only one id of each player,
+-- so we group the id and make sure the theres only one count of the id that prints its first log in. 
