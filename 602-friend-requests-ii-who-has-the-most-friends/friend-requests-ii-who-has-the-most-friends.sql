f9ac00b27717b883e5f2719de22faa65c9ffc833
Davis Vang <165615909+DavisVang@users.noreply.github.com>
2026-07-18 15:22:30 -0500

    Update friend-requests-ii-who-has-the-most-friends.sql

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -5,3 +5,5 @@ select accepter_id as id from RequestAccepted) ids
 group by id
 order by num desc
 limit 1;
+
+-- in the subquery, we connect the two columns into one by union all because we want the duplicates. Then on the outer queries allow us to group the id and count each indinivudal friend. 
