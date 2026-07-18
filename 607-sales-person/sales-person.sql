a42b187fc54dc8d9dfcec6ccaf1c21a3de0d5b95
Davis Vang <165615909+DavisVang@users.noreply.github.com>
2026-07-18 16:36:24 -0500

    Update sales-person.sql

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -5,3 +5,7 @@ From Orders
 Join Company 
 on Orders.com_id = Company.com_id
 where Company.name = 'red');
+
+-- We want to join company and order table together to filter out the sales person who has had work with RED which is (1,4). 
+-- Then we use that to filter out in the SalesPerson Table with a subquery, asking each sales id if it is not 1 or 4. 
+-- It returns the names that its id is not 1 or 4. 
